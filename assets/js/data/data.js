ec61dc70e1b5bc6f0fdb172b854cc1b9109ca5c2
unknown <joys23services@gmail.com>
2024-05-01 22:14:03 -0400

    pagina creada

---
+++ assets/js/data/data.js
@@ -0,0 +1,82 @@
+export const items =[
+    {
+      "id": 1,
+      "titulo": "Hamburguesa Clásica",
+      "imagen": "./assets/img/clasica.png",
+      "precio": 5.99,
+      "quantity": 10,
+      "detalles": "Deliciosa hamburguesa con carne de res, lechuga, tomate y queso"
+    },
+    {
+      "id": 2,
+      "titulo": "Hamburguesa BBQ",
+      "imagen": "./assets/img/bbq.png",
+      "precio": 6.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa con carne de res, salsa BBQ, cebolla caramelizada y queso"
+    },
+    {
+      "id":3,
+      "titulo": "Hamburguesa Ranchera",
+      "imagen": "./assets/img/Ranchera.png",
+      "precio": 7.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa con carne de res, salsa ranchera, jalapeños y queso"
+    },
+    {
+      "id": 4,
+      "titulo": "Hamburguesa Veggie",
+      "imagen": "./assets/img/veggie.png",
+      "precio": 6.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa vegetariana con base de lentejas, zanahoria y especias"
+    },
+    {
+      "id": 5,
+      "titulo": "Hamburguesa Doble",
+      "imagen": "./assets/img/doble.png",
+      "precio": 8.99,
+      "quantity": 10,
+      "detalles": "Doble carne de res, doble queso, lechuga, tomate y cebolla"
+    },
+    {
+      "id": 6,
+      "titulo": "Hamburguesa Hawaiana",
+      "imagen": "./assets/img/hawaiana.png",
+      "precio": 7.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa con carne de res, piña, jamón, queso y salsa especial"
+    },
+    {
+      "id": 7,
+      "titulo": "Hamburguesa de Pollo",
+      "imagen": "./assets/img/pollo.png",
+      "precio": 6.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa con pollo empanizado, lechuga, tomate y mayonesa"
+    },
+    {
+      "id": 8,
+      "titulo": "Hamburguesa de Pescado",
+      "imagen": "./assets/img/pescado.png",
+      "precio": 7.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa con filete de pescado empanizado, lechuga, tomate y salsa tártara"
+    },
+    {
+      "id": 9,
+      "titulo": "Hamburguesa Mexicana",
+      "imagen": "./assets/img/mexicana.png",
+      "precio": 7.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa con carne de res, guacamole, jalapeños, cebolla y queso"
+    },
+    {
+      "id": 10,
+      "titulo": "Hamburguesa de Tofu",
+      "imagen": "./assets/img/tofu.png",
+      "precio": 6.99,
+      "quantity": 10,
+      "detalles": "Hamburguesa vegana con base de tofu, espinacas y especias"
+    }
+  ]
